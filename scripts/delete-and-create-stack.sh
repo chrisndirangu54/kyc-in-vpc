bdceaee23dcccab4dc32b8b7a25040d414418d4f
Mark Vayngrib <mark@tradle.io>
2018-09-22 18:18:40 -0400

    update AMIs, use params for paths, add outputs for api urls

--- scripts/delete-and-create-stack.sh
+++ scripts/delete-and-create-stack.sh
@@ -5,6 +5,13 @@ set -euo pipefail
 
 source "$(dirname $0)/env.sh"
 
+read -p "This will DELETE your stack, which is irreversible. Continue (y/n)?" choice
+case "$choice" in
+  y|Y ) echo "deleting...";;
+  n|N ) exit 0;;
+  * ) exit 0;;
+esac
+
 aws cloudformation delete-stack --stack-name "$STACK_NAME"
 aws cloudformation wait stack-delete-complete --stack-name "$STACK_NAME"
 "$(dirname $0)/create-or-update-stack.sh"
