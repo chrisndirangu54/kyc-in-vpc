f1224ecbd07dbed5c5fbab76ffc1a76398742fd2
Mark Vayngrib <mark@tradle.io>
2018-09-23 13:41:56 -0400

    untrack env.sh

--- scripts/env-sample.sh
+++ scripts/env-sample.sh
@@ -1,3 +1,3 @@
-AWS_PROFILE=default
-STACK_NAME=MainnetParity1
-BUCKET=my-lambda-code-bucket
+export AWS_PROFILE=default
+export STACK_NAME=kyc-services
+export BUCKET=my-s3-bucket-to-save-templates-to
--- scripts/env.sh
+++
@@ -1,3 +0,0 @@
-export AWS_PROFILE=mv
-export STACK_NAME=kyc-services
-export BUCKET=mvayngrib-serverless-deployment
