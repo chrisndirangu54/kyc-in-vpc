4d11bb9e662c91921fbced7c7655215a0d1bdf4a
Mark Vayngrib <mark@tradle.io>
2018-09-26 10:27:35 -0400

    add update-amis script

--- scripts/cli.js
+++ scripts/cli.js
@@ -1,9 +1,42 @@
 #!/usr/bin/env node
 
+const { promisify } = require('util')
+const fs = require('fs')
+const path = require('path')
 const AWS = require('aws-sdk')
 const yargs = require('yargs')
 const cloneDeep = require('lodash/cloneDeep')
+const Promise = require('bluebird')
+const YAML = require('js-yaml')
+// const schema = require('cloudformation-js-yaml-schema').genSchema(YAML)
 const log = (...args) => console.log(...args)
+const paths = {
+  ecs: path.resolve(__dirname, '../cloudformation/ecs.yml'),
+  bastion: path.resolve(__dirname, '../cloudformation/bastion.yml'),
+}
+
+const _readFile = promisify(fs.readFile.bind(fs))
+const _writeFile = promisify(fs.writeFile.bind(fs))
+const readFile = filePath => _readFile(filePath, { encoding: 'utf8' })
+const writeFile = (filePath, value) => _writeFile(filePath, value, { encoding: 'utf8' })
+const regions = [
+  'us-east-2',
+  'us-east-1',
+  'us-west-2',
+  'us-west-1',
+  'eu-west-3',
+  'eu-west-2',
+  'eu-west-1',
+  'eu-central-1',
+  'ap-northeast-2',
+  'ap-northeast-1',
+  'ap-southeast-2',
+  'ap-southeast-1',
+  'ca-central-1',
+  'ap-south-1',
+  'sa-east-1',
+  'us-gov-west-1',
+]
 
 const BASE_POLICY = {
   "Version": "2008-10-17",
@@ -31,6 +64,87 @@ const addPullAccess = ({ policy, iamArn }) => {
 }
 
 const getIamArnForAccount = account => `arn:aws:iam::${account}:root`
+
+const byCreationDateDesc = (a, b) => new Date(b.CreationDate) - new Date(a.CreationDate)
+
+const getLatestECSImage = async ({ region }) => {
+  const ec2 = getEC2ClientByRegion(region)
+  const { Images } = await ec2.describeImages({
+    Filters: [
+      {
+        Name: 'owner-alias',
+        Values: [
+          'amazon',
+        ]
+      },
+      {
+        Name: 'name',
+        Values: [
+          '*ecs-optimized*',
+        ]
+      },
+      {
+        Name: 'state',
+        Values: [
+          'available'
+        ]
+      }
+    ]
+  }).promise()
+
+  const latest = Images.sort(byCreationDateDesc)[0]
+  return latest
+}
+
+const getEC2ClientByRegion = region => {
+  AWS.config.update({ region })
+  return new AWS.EC2()
+}
+
+// const getClientByRegion = (() => {
+//   const cache = {}
+//   return region => {
+//     if (!region) {
+//       const client = cache[region] = new AWS()
+//       client.config.update({ region })
+//     }
+
+//     return cache[region]
+//   }
+// })();
+
+const updateAMIs = async () => {
+  const latestAMIs = await Promise.mapSeries(regions, region => getLatestECSImage({ region }))
+  const byRegion = latestAMIs.reduce((map, image, i) => {
+    map[regions[i]] = {
+      AMI: image.ImageId
+    }
+
+    return map
+  }, {})
+
+  const amisYml = YAML.safeDump({
+    AWSRegionToAMI: byRegion
+  })
+  .split('\n')
+  .map(line => '  ' + line)
+  .join('\n')
+
+  await Promise.map([paths.ecs, paths.bastion], async filePath => {
+    const yml = await readFile(filePath)
+    const start = yml.indexOf('\n# START_AMIS')
+    const end = yml.indexOf('# END_AMIS')
+    const before = yml.slice(0, start)
+    const middle = amisYml
+    const after = yml.slice(end)
+    await writeFile(filePath, [
+      before,
+      '# START_AMIS',
+      middle,
+      after
+    ].join('\n'))
+  })
+}
 
 class ECRWrapper {
   constructor(client) {
@@ -108,6 +222,19 @@ yargs
       },
       account: {
         type: 'string',
+      },
+    }
+  })
+  .command({
+    command: 'update-amis',
+    desc: 'update amis in template',
+    handler: ({ region }) => {
+      AWS.config.update({ region })
+      return updateAMIs()
+    },
+    builder: {
+      region: {
+        default: 'us-east-1',
       },
     }
   })
