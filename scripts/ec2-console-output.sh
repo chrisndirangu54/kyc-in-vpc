6b84a1fb7798089e47c4e692679f2e56152747d9
Mark Vayngrib <mvayngrib@gmail.com>
2018-12-02 16:08:23 -0500

    refactor: allow to load license on demand from s3, vs baked into image

--- scripts/ec2-console-output.sh
+++ scripts/ec2-console-output.sh
@@ -6,4 +6,4 @@ set -x
 source "$(dirname "$0")/env.sh"
 
 EC2_INSTANCE=$("$(dirname $0)/get-container-instance.sh")
-aws ec2 get-console-output --instance-id "$EC2_INSTANCE"
+aws ec2 get-console-output --instance-id "$EC2_INSTANCE" | jq -r .Output
